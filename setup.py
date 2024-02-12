bb3f37f4a2a8df5a556c67f07d3d6414ab962965
Rajesh Khangar <rkhangar1998@gmail.com>
2024-02-12 21:45:14 +0530

    add cookicutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='implimenting ci cd in creditcard project',
+    author='rajesh',
+    license='',
+)
